87b6eade5e202494cd834a8a1c411d13741013a9
mireiavh <mirvazher02@gmail.com>
2023-08-18 15:26:22 +0200

    Css vanilla navbar not finished added v.1.2

--- app/views/view_new_coder_form.php
+++ app/views/view_new_coder_form.php
@@ -10,19 +10,21 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="../../public/css/view_coder_form.css" rel="stylesheet">
+    <link href="../../public/css/coder_form.css" rel="stylesheet">
+    <link href="../../public/css/navbar.css" rel="stylesheet">
     <title>CoderFlow_</title>
 </head>
 
 <header>
     <nav>
-        <div class="nav_container_1">
+        <div class="nav_container">
+        <div class="nav_tab nav_tab_1">Pestaña 1</div>
+        <div class="nav_tab nav_tab_2">Pestaña 2</div>
+        <div class="nav_tab nav_tab_3">Pestaña 3</div>
+            </div> 
         </div>
-        <div class="nav_container_2">
-        </div>
-        <div class="nav_container_3">
-        </div> 
     </nav>
+
 </header>
 
 <body>
@@ -39,20 +41,26 @@
                     <img src="../../src/assets/user_photo.png" alt="coder_photo" class="img_coder">
                 </div>
 
-                <!--botón-->
-                <input type="submit" value="Volver" class="btn_1">
-                <input type="submit" value="Modificar" class="btn_2">
+                <div class="btn_container">
+                    <div>
+                        <input type="submit" value="Volver" class="btn_1">
+                    </div>
+                    
+                    <div>
+                        <input type="submit" value="Modificar" class="btn_2">
+                    </div>
+                </div>
 
             </article>
 
             <form action="" method="POST">
                 
-                <section>
+                <section class="input_container">
                     <label for="id_coder" class="name_label">ID coder</label>
                     <input type="text" name="id_coder" value="" class="input_form" required>
 
                     <label class="name_label">Edad</label>
-                    <select name="gender" class="input_form" required>
+                    <select name="gender" class="short_form" required>
                         <option value="0"></option>
                         <option value="1">18-29</option>
                         <option value="2">30-45</option>
@@ -66,28 +74,31 @@
                     <label for="last_name" class="name_label">Apellidos</label>
                     <input type="text" name="last_name" value="" class="input_form" required><br><br>
 
-                    <label class="name_label">Género</label>
-                    <select name="gender" class="input_form" required>
-                        <option value="0"></option>
-                        <option value="1">Mujer</option>
-                        <option value="2">Hombre</option>
-                        <option value="3">Otros</option>
-                    </select><br><br>
+                    <section class="group_container">
+                        <label class="name_label">Género</label>
+                        <select name="gender" class="short_form" required>
+                            <option value="0"></option>
+                            <option value="1">Mujer</option>
+                            <option value="2">Hombre</option>
+                            <option value="3">Otro</option>
+                            <option value="4">SIN (Sin especificar)</option>
+                        </select><br><br>
 
-                    <label class="name_label">Discapacidad</label>
-                    <select name="disability" class="input_form" required>
-                        <option value="0"></option>
-                        <option value="1">Sí</option>
-                        <option value="2">No</option>
-                        <option value="3">Pendiente</option>
-                    </select><br><br>
+                        <label class="name_label">Discapacidad</label>
+                        <select name="disability" class="short_form" required>
+                            <option value="0"></option>
+                            <option value="1">Sí</option>
+                            <option value="2">No</option>
+                            <option value="3">P (Pendiente de revisión)</option>
+                        </select><br><br>
+                    </section>
 
                     <label for="documents" class="name_label">Documentos</label>
                     <input type="text" name="documents" value="" class="input_form" required><br><br>
 
                 </section>
 
-                <section>
+                <section class="input_container_2">
 
                     <label for="dni_nief" class="name_label">DNI/NIEF</label>
                     <input type="text" name="dni_nief" value="" class="input_form" required><br><br>
